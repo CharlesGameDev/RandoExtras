7e84c63e20fad58bee147afe7ab8b63994d89969
CharlesGameDev <99226468+CharlesGameDev@users.noreply.github.com>
2023-05-06 03:06:20 -0400

    Add project files.

---
+++ RandoExtras.cs
@@ -0,0 +1,15 @@
+﻿using Modding;
+using System.Collections;
+
+namespace RandoExtras
+{
+    public class RandoExtras : Mod
+    {
+        new public string GetName() => "RandoExtras";
+        public override string GetVersion() => "1.0.0.0";
+        public override void Initialize()
+        {
+
+        }
+    }
+}
